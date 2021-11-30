ff9be85f53bde2e19975133f17ad2085d76c9229
Brian Day <brianday@Brians-MacBook-Pro.local>
2021-11-30 15:48:06 -0500

    Updated setup.py and renamed directory to reflect module name change.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 setup(
-    name = 'lammps_tools',
+    name = 'atomaton',
     version = '0.1.0',
     description = 'Tools for building lammps datafiles',
     author = 'Brian A Day',
     author_email = 'brd84@pitt.edu',
-    url = 'https://github.com/birdday/LAMMPS_tools',
-    packages = ['lammps_tools'],
-    install_requires=['numpy','ase'],
+    url = 'https://github.com/birdday/atomaton',
+    packages = ['atomaton'],
+    install_requires=['numpy', 'ase', 'mayavi'],
     tests_require=['pytest']
 )
