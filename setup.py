7d8f8f4d7d22361a5fbbd6e280b94b8807073eec
Brian Day <22bday@gmail.com>
2024-12-30 02:08:25 -0500

    Try more explicit import ordering...

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email="22bday@gmail.com",
     url="https://github.com/birdday/atomaton",
     packages=["atomaton"],
-    install_requires=["numpy", "ase", "vtk==9.2.6", "mayavi", "imageio", "pyqt5==5.15.10"],
+    install_requires=["numpy", "ase", "vtk==9.2.6", "mayavi", "imageio", "PyQt5-sip", "PyQt5==5.15.10"],
     tests_require=["pytest"],
 )
