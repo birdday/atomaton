b704c25781e9d66fa72fcaac44ad5623f5b7d7d5
Brian Day <22bday@gmail.com>
2025-01-01 01:12:20 -0500

    Try updated vtk.

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author_email="22bday@gmail.com",
     url="https://github.com/birdday/atomaton",
     packages=["atomaton"],
-    install_requires=["numpy", "ase", "vtk==9.2.6", "mayavi", "imageio", "PyQt5"],
+    install_requires=["numpy", "ase", "vtk", "mayavi", "imageio", "PyQt5"],
     tests_require=["pytest"],
 )
