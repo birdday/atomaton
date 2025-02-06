4299004bde3b0720a6b7b607d607faba018b8607
Brian Day <22bday@gmail.com>
2025-02-06 15:38:45 -0500

    Modify setup and docs build.

--- setup.py
+++ setup.py
@@ -10,8 +10,9 @@ setup(
     author_email="22bday@gmail.com",
     url="https://github.com/birdday/atomaton",
     packages=["atomaton"],
-    # Other versions may work, but graphics have been tempermental for me.
-    install_requires=["numpy==1.22.4", "ase", "vtk==9.2.6", "mayavi==4.8.1", "imageio", "PyQt5", "configobj",
+    # NOTE: Pinning version of mayavi causes sphinx docs to fail. On ARM Mac, I use mayavi==4.8.1.
+    # Newer combinations of packages may work, but require some troubleshooting.
+    install_requires=["numpy==1.22.4", "ase", "vtk==9.2.6", "mayavi", "imageio", "PyQt5", "configobj",
                       "matplotlib==3.8.2"],
     tests_require=["pytest"],
 )
