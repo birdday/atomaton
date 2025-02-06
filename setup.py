f4fee167135c07ab9ed30f8ead1ddea731809c55
Brian Day <22bday@gmail.com>
2025-02-06 15:18:44 -0500

    Fixed versions for setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     author_email="22bday@gmail.com",
     url="https://github.com/birdday/atomaton",
     packages=["atomaton"],
-    install_requires=["numpy", "ase", "vtk==9.2.6", "mayavi", "imageio", "PyQt5"],
+    # Other versions may work, but graphics have been tempermental for me.
+    install_requires=["numpy==1.22.4", "ase", "vtk==9.2.6", "mayavi==4.8.1", "imageio", "PyQt5", "configobj",
+                      "matplotlib==3.8.2"],
     tests_require=["pytest"],
 )
